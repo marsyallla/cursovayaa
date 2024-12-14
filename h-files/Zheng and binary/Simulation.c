8632701c763303e99149029354e22790e5f7152e
marsyallla <mariyastalinskaya@gmail.com>
2024-12-15 02:24:15 +0300

    new

--- h-files/Zheng and binary/Simulation.c
+++ h-files/Zheng and binary/Simulation.c
@@ -1,8 +1,8 @@
 #include "simulation.h"
-#include "search.h"
 #include <stdlib.h>
 #include <time.h>
 
+// Нормализация вероятностей
 void normalize(float* probability, int size) {
     float sum = 0.0;
     for (int i = 0; i < size; i++) {
@@ -13,6 +13,7 @@ void normalize(float* probability, int size) {
     }
 }
 
+// Создание массива накопленных вероятностей
 void allsum(float* probability, float* allsums, int size) {
     allsums[0] = probability[0];
     for (int i = 1; i < size; i++) {
@@ -20,25 +21,68 @@ void allsum(float* probability, float* allsums, int size) {
     }
 }
 
-void simulate(int n, float* probability, int y, int* result, int method, float* random) {
+// Инициализация вспомогательного массива r_j
+void initialize_r(int n, int m, float* s, int* r) {
+    int i = 0;
+    for (int j = 1; j <= m; j++) {
+        while (i < n && s[i] < (float)(j - 1) / m) {
+            i++;
+        }
+        r[j - 1] = i;
+    }
+}
+
+// Выборка методом Чжень
+int zhen_sample(int n, int m, float* s, int* r) {
+    float alpha = (float)rand() / RAND_MAX; // Случайное число
+    int j = (int)(m * alpha);              // Индекс для r_j
+    if (j >= m) j = m - 1;
+
+    int i = r[j]; // Начальный индекс
+    while (i < n && alpha > s[i]) {
+        i++;
+    }
+    return i;
+}
+
+// Симуляция методом Чжень
+void simulate_zhen(int n, float* probability, int y, int* result, float* random) {
+    float* allsums = (float*)malloc(n * sizeof(float));
+    int* r = (int*)malloc(y * sizeof(int));
+
+    allsum(probability, allsums, n);
+    initialize_r(n, y, allsums, r);
+
+    for (int i = 0; i < y; i++) {
+        int index = zhen_sample(n, y, allsums, r);
+        result[index]++;
+    }
+
+    free(allsums);
+    free(r);
+}
+
+// Симуляция бинарным поиском
+int binary(float* allsums, int size, float xi) {
+    int left = 0, right = size - 1;
+    while (left < right) {
+        int mid = (left + right) / 2;
+        if (allsums[mid] < xi)
+            left = mid + 1;
+        else
+            right = mid;
+    }
+    return left;
+}
+
+void simulate_binary(int n, float* probability, int y, int* result, float* random) {
     float* allsums = (float*)malloc(n * sizeof(float));
     allsum(probability, allsums, n);
 
-    if (method == 0) {  // Binary Search
-        for (int i = 0; i < y; i++) {
-            float xi = random[i];
-            int ind = binary(allsums, n, xi);
-            result[ind]++;
-        }
-    } else {  // Zhen Search
-        for (int i = 0; i < y; i++) {
-            float xi = random[i];
-            int index = 0;
-            while (index < n && allsums[index] < xi) {
-                index++;
-            }
-            result[index]++;
-        }
+    for (int i = 0; i < y; i++) {
+        float xi = random[i];
+        int ind = binary(allsums, n, xi);
+        result[ind]++;
     }
 
     free(allsums);
--- h-files/Zheng and binary/Simulation.h
+++ h-files/Zheng and binary/Simulation.h
@@ -3,6 +3,9 @@
 
 void normalize(float* probability, int size);
 void allsum(float* probability, float* allsums, int size);
-void simulate(int n, float* probability, int y, int* result, int method, float* random);
+void initialize_r(int n, int m, float* s, int* r);
+int zhen_sample(int n, int m, float* s, int* r);
+void simulate_binary(int n, float* probability, int y, int* result, float* random);
+void simulate_zhen(int n, float* probability, int y, int* result, float* random);
 
 #endif
